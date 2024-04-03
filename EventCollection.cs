8ea11cff1a066e7f5de630e9acd03df8fe1a6d57
solutena <solutena@naver.com>
2024-04-03 18:57:36 +0900

    Update EventCollection.cs

--- EventCollection.cs
+++ EventCollection.cs
@@ -4,48 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public delegate void EventCollectionEventHandler<T>(T item, bool isAdd);
 interface IEventCollection<T>
 {
-	public event Action<T, bool> Event;
+	public event EventCollectionEventHandler<T> Event;
 	public event Action Changed;
 }
 
 [Serializable]
 public class EventDictionary<Key, Value> : IEventCollection<Value>, IEnumerable<KeyValuePair<Key, Value>>, ISerializationCallbackReceiver
 {
-	[SerializeField] List<KeyValuePair<Key, Value>> serialize;
-	Dictionary<Key, Value> dictionary = new Dictionary<Key, Value>();
+	[SerializeField] SerializebleDictionary<Key, Value> serialize;
 
-	public event Action<Value, bool> Event;
+	public event EventCollectionEventHandler<Value> Event;
 	public event Action Changed;
 
-	public Value this[Key key] => dictionary[key];
+	public Value this[Key key] => serialize[key];
 	public EventDictionary() { }
 
-	public void OnBeforeSerialize()
-	{
-		serialize = dictionary.ToList();
-	}
-
+	public void OnBeforeSerialize() { }
 	public void OnAfterDeserialize()
 	{
-		dictionary = serialize.ToDictionary(pair => pair.Key, pair => pair.Value);
-		foreach (var value in dictionary.Values)
-			Event?.Invoke(value, true);
+		foreach (var pair in serialize)
+			Event?.Invoke(pair.Value, true);
 		Changed?.Invoke();
 	}
 
 	public void Add(Key key, Value value)
 	{
-		dictionary.Add(key, value);
+		serialize.Add(key, value);
 		Event?.Invoke(value, true);
 		Changed?.Invoke();
 	}
 
 	public void Clear()
 	{
-		var prev = new List<Value>(dictionary.Values);
-		dictionary.Clear();
+		var prev = serialize.Values.ToList();
+		serialize.Clear();
 		foreach (var item in prev)
 			Event?.Invoke(item, false);
 		Changed?.Invoke();
@@ -53,8 +48,8 @@ public class EventDictionary<Key, Value> : IEventCollection<Value>, IEnumerable<
 
 	public bool Remove(Key key)
 	{
-		var value = dictionary[key];
-		if (dictionary.Remove(key))
+		var value = serialize[key];
+		if (serialize.Remove(key))
 		{
 			Event?.Invoke(value, false);
 			Changed?.Invoke();
@@ -63,40 +58,37 @@ public class EventDictionary<Key, Value> : IEventCollection<Value>, IEnumerable<
 		return false;
 	}
 
-	public bool ContainsKey(Key key) => dictionary.ContainsKey(key);
-	public Value Get(Key key) => dictionary.Get(key);
-	public IEnumerator<KeyValuePair<Key, Value>> GetEnumerator() => dictionary.GetEnumerator();
-	IEnumerator IEnumerable.GetEnumerator() => dictionary.GetEnumerator();
+	public void OnChanged() => Changed?.Invoke();
+	public void OnEvent(Value value, bool isAdd) => Event?.Invoke(value, isAdd);
+	public bool ContainsKey(Key key) => serialize.ContainsKey(key);
+	public Value Get(Key key) => serialize.Get(key);
+	public IEnumerator<KeyValuePair<Key, Value>> GetEnumerator() => serialize.GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => serialize.GetEnumerator();
 }
+
 
 [Serializable]
 public class EventHashSet<T> : IEventCollection<T>, IEnumerable<T>, ISerializationCallbackReceiver
 {
-	[SerializeField] List<T> serialize;
-	HashSet<T> hashSet = new HashSet<T>();
+	[SerializeField] SerializebleHashSet<T> serialize;
 
-	public int Count => hashSet.Count;
-	public event Action<T, bool> Event;
+	public int Count => serialize.Count;
+	public event EventCollectionEventHandler<T> Event;
 	public event Action Changed;
 
 	public EventHashSet() { }
 
-	public void OnBeforeSerialize()
-	{
-		serialize = hashSet.ToList();
-	}
-
+	public void OnBeforeSerialize() { }
 	public void OnAfterDeserialize()
 	{
-		hashSet = serialize.ToHashSet();
-		foreach (var item in hashSet)
+		foreach (var item in serialize)
 			Event?.Invoke(item, true);
 		Changed?.Invoke();
 	}
 
 	public bool Add(T item)
 	{
-		if (hashSet.Add(item))
+		if (serialize.Add(item))
 		{
 			Event?.Invoke(item, true);
 			Changed?.Invoke();
@@ -107,8 +99,8 @@ public class EventHashSet<T> : IEventCollection<T>, IEnumerable<T>, ISerializati
 
 	public void Clear()
 	{
-		var prev = new List<T>(hashSet);
-		hashSet.Clear();
+		var prev = serialize.ToList();
+		serialize.Clear();
 		foreach (var item in prev)
 			Event?.Invoke(item, false);
 		Changed?.Invoke();
@@ -116,7 +108,7 @@ public class EventHashSet<T> : IEventCollection<T>, IEnumerable<T>, ISerializati
 
 	public bool Remove(T item)
 	{
-		if (hashSet.Remove(item))
+		if (serialize.Remove(item))
 		{
 			Event?.Invoke(item, false);
 			Changed?.Invoke();
@@ -125,9 +117,11 @@ public class EventHashSet<T> : IEventCollection<T>, IEnumerable<T>, ISerializati
 		return false;
 	}
 
-	public bool Contains(T item) => hashSet.Contains(item);
-	public IEnumerator<T> GetEnumerator() => hashSet.GetEnumerator();
-	IEnumerator IEnumerable.GetEnumerator() => hashSet.GetEnumerator();
+	public void OnChanged() => Changed?.Invoke();
+	public void OnEvent(T item, bool isAdd) => Event?.Invoke(item, isAdd);
+	public bool Contains(T item) => serialize.Contains(item);
+	public IEnumerator<T> GetEnumerator() => serialize.GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => serialize.GetEnumerator();
 }
 
 [Serializable]
@@ -138,17 +132,20 @@ public class EventList<T> : IEventCollection<T>, IEnumerable<T>, ISerializationC
 	public T this[int index] => serialize[index];
 
 	public int Count => serialize.Count;
-	public event Action<T, bool> Event;
+	public event EventCollectionEventHandler<T> Event;
 	public event Action Changed;
 
 	public EventList() { }
 	public EventList(IEnumerable<T> collection) { AddRange(collection); }
 
 	public void OnBeforeSerialize() { }
-	public void OnAfterDeserialize() { serialize.ForEach(item => Event?.Invoke(item, true)); Changed?.Invoke(); }
-	public void OnChanged() => Changed?.Invoke();
-	public void OnEvent(T item, bool isAdd) => Event?.Invoke(item, isAdd);
-
+	public void OnAfterDeserialize()
+	{
+		foreach (var item in serialize)
+			Event?.Invoke(item, true);
+		Changed?.Invoke();
+	}
+	
 	public void Add(T item)
 	{
 		serialize.Add(item);
@@ -204,6 +201,8 @@ public class EventList<T> : IEventCollection<T>, IEnumerable<T>, ISerializationC
 		Changed?.Invoke();
 	}
 
+	public void OnChanged() => Changed?.Invoke();
+	public void OnEvent(T item, bool isAdd) => Event?.Invoke(item, isAdd);
 	public void ForEach(Action<T> action) => serialize.ForEach(action);
 	public bool Contains(T item) => serialize.Contains(item);
 	public IEnumerator<T> GetEnumerator() => serialize.GetEnumerator();
